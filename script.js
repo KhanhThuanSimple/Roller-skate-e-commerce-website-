2f68649b312c9b96cd230fa2a2668845588c08db
KhanhThuanSimple <22130277@st.hcmuaf.edu.vn>
2024-11-20 20:37:45 +0700

    update

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-// Lấy phần modal
 var modal = document.getElementById("productModal");
 
-// Lấy nút "Đặt hàng nhanh" và nút "Open Modal"
 var quickOrderBtn = document.getElementById("quickOrderBtn");
 var openModalBtn = document.getElementById("myBtn");
 
-// Lấy phần "close" để đóng modal
 var closeModal = document.getElementsByClassName("close")[0];
 
-// Mở modal khi nhấn vào nút "Đặt hàng nhanh"
 quickOrderBtn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Mở modal khi nhấn vào nút "Open Modal"
 openModalBtn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Đóng modal khi nhấn vào dấu 'x'
 closeModal.onclick = function() {
     modal.style.display = "none";
 }
 
-// Đóng modal khi nhấn ra ngoài modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+const pageItems = document.querySelectorAll('.page-item');
+
+const currentPage = window.location.pathname.split('/').pop();
+
+pageItems.forEach(item => {
+  if (item.getAttribute('href') === currentPage) {
+    item.classList.add('active'); 
+  }
+});
+function sortProducts(order) {
+    const products = [...document.querySelectorAll('.product li')];
+    products.sort((a, b) => {
+        const priceA = parseInt(a.querySelector('.product-price').textContent.replace(/[^0-9]/g, ''));
+        const priceB = parseInt(b.querySelector('.product-price').textContent.replace(/[^0-9]/g, ''));
+        return order === 'asc' ? priceA - priceB : priceB - priceA;
+    });
+    const productList = document.querySelector('.product');
+    productList.innerHTML = '';
+    products.forEach(product => productList.appendChild(product));
 }
