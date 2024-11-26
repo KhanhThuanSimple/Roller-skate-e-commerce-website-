1a42df0afc911b6b6adb7b1a5aaca3f89e6d73e7
Huyền Như <22130198@st.hcmuaf.edu.vn>
2024-11-26 13:45:27 +0700

    update don hang

--- donhang.js
+++ donhang.js
@@ -1,4 +1,17 @@
-// Xử lí nút xem chi tiết
-document.getElementById('XemChiTietDonHang12345').addEventListener('click', function () {
-    window.location.href = 'chitietdonhang.html';
-});
+// Mở cửa sổ xem chi tiết đơn hàng
+function moCTDH1() {
+    document.getElementById("modal-CTDH1").style.display = "block";
+}
+function moCTDH2() {
+    document.getElementById("modal-CTDH2").style.display = "block";
+}
+
+
+// Đóng cửa sổ xem chi tiết đơn hàng
+function dongCTDH1() {
+    document.getElementById("modal-CTDH1").style.display = "none";
+}
+function dongCTDH2() {
+    document.getElementById("modal-CTDH2").style.display = "none";
+}
+
