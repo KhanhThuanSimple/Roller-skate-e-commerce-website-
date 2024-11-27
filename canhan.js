97731ac0a8fe9137a400d7bdc19f46669a7e5911
Huyền Như <22130198@st.hcmuaf.edu.vn>
2024-11-27 23:31:26 +0700

    update dang xuat
    viết khung bạn chắc chắn muốn đăng xuất

--- canhan.js
+++ canhan.js
@@ -1,3 +1,13 @@
-document.getElementById('DangXuat').addEventListener('click', function () {
+// phần "bạn chắc chắn đăng xuất?"
+function moChacChanDX() {
+    document.getElementById("modal_dangxuat").style.display = "block";
+}
+
+function dongChacChanDX() {
+    document.getElementById("modal_dangxuat").style.display = "none";
+}
+
+// xử lí khi user chọn chắc chắn đăng xuất
+document.getElementById('chacChanDX').addEventListener('click', function () {
     window.location.href = 'login.html';
 });
